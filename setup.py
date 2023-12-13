c8922f2cb353d5e87d79299d903d8ddb972d5315
Abdelilah HSSASSA <abdelilah.hssassa@etu.uae.ac.ma>
2023-12-13 12:15:04 +0100

    Second commit

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str) -> List[str]:
+
+    '''
+        This function will return a list of libraries / packages required to run the application
+    '''
+    e = '-e .'
+    with open(file_path) as file:
+        requirements = file.readline()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if e in requirements:
+            requirements.remove(e)
+
+    return requirements
 
 setup(
 
@@ -7,6 +24,6 @@ setup(
     author = 'HSSASSA',
     author_email ='abdelilah.hssassa@etu.uae.ac.ma',
     packages = find_packages(),
-    install_requires = ['pandas','numpy','seaborn']
+    install_requires = get_requirements('requirements.txt')
 
 )
